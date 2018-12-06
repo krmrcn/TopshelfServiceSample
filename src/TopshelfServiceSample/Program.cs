952b5974da2dab0544df0f6ae6be10ad68cea65c
Kerem <krmrcn@users.noreply.github.com>
2018-12-07 01:04:51 +0300

    initial

---
+++ src/TopshelfServiceSample/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Topshelf;
+
+namespace TopshelfServiceSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var exitCode = HostFactory.Run(d =>
+            {
+                d.Service<SystemChecker>(s =>
+                {
+                    s.ConstructUsing(checker => new SystemChecker());
+                    s.WhenStarted(checker => checker.OnStart());
+                    s.WhenStopped(checker => checker.OnStop());
+                });
+
+                d.RunAsLocalSystem();
+
+                d.SetServiceName("SysChecker");
+                d.SetDisplayName("System Checker");
+                d.SetDescription("Topshelf Sample");
+            });
+
+            int exitCodeValue = (int)Convert.ChangeType(exitCode, exitCode.GetTypeCode());
+            Environment.ExitCode = exitCodeValue;
+        }
+    }
+}
---
+++ src/TopshelfServiceSample/SystemChecker.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Timers;
+
+namespace TopshelfServiceSample
+{
+    public class SystemChecker
+    {
+        private readonly Timer _timer;
+
+        public SystemChecker()
+        {
+            _timer = new Timer(2000) { AutoReset = true };
+            _timer.Elapsed += TimerElapsed;
+            WriteLine(DateTime.Now.Ticks + " Started...");
+        }
+
+        private void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            /*
+            more here...
+            */
+
+            WriteLine(DateTime.Now.Ticks + " OK");
+        }
+
+        public void OnStart()
+        {
+            _timer.Start();
+        }
+
+        public void OnStop()
+        {
+            _timer.Stop();
+            WriteLine(DateTime.Now.Ticks + " Stopped...");
+        }
+
+        private void WriteLine(string text)
+        {
+            var status = new string[] { text };
+            File.AppendAllLines(@"C:\test\Monitor.txt", status);
+        }
+    }
+}
